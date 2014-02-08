5222af511520a513471c7d06739c045d8d220a58
liyustar <liyustar@gmail.com>
2014-02-09 01:34:28 +0800

    add wrap functions

--- intro/daytimetcpsrv.c
+++ intro/daytimetcpsrv.c
@@ -9,19 +9,19 @@ main(int argc, char **argv)
 	char buff[MAXLINE];
 	time_t ticks;
 
-	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons(13);	/* daytime server */
+	servaddr.sin_port = htons(SERV_PORT);	/* daytime server */
 
-	bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
-	listen(listenfd, LISTENQ);
+	Listen(listenfd, LISTENQ);
 
 	for ( ; ; ) {
-		connfd = accept(listenfd, (SA *) NULL, NULL);
+		connfd = Accept(listenfd, (SA *) NULL, NULL);
 
 		ticks = time(NULL);
 		snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
